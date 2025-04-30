91775c930837e3ed355c2adff654a21f4cbd4104
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-04-30 17:28:10 +0600

    change folder and update code

---
+++ Randome code/condition.js
@@ -0,0 +1,9 @@
+console.log("Conditionals in JavaScript");
+// 1. if statement
+
+let age = 15;
+if (age >= 10 && age <= 20) {
+    console.log("Age is between 10 and 20");
+} else {
+    console.log("Age is not between 10 and 20");
+}
---
+++ Randome code/string-number-concat.js
@@ -0,0 +1,10 @@
+// Problem 02: create a variable of type string and try to add a number to it.
+
+let myString = "Hello, I am Hafizur Rahman ";
+let myNumber = 25;
+
+// Add number to string (concatenation)
+let result = myString + myNumber;
+
+console.log(result); // Output: "Hello, I am Hafizur Rahman 25"
+console.log(typeof result); // Output: "string"
