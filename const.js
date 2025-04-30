4322997fa8f654296ae073bc52422db36bf70bc7
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-04-30 16:41:40 +0600

    Create const.js

---
+++ const.js
@@ -0,0 +1,6 @@
+// constant of js 
+console.log("constant learning");
+var a = 10; // variable declaration and initialization
+var b = 20; // variable declaration and initialization
+var c = a + b; // variable declaration and initialization
+console.log(c); // Output: 30
