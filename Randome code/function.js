2f895d270e54b552b7f7853f6416cb08edd4b21b
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-05-01 17:36:25 +0600

    function code

---
+++ Randome code/function.js
@@ -0,0 +1,12 @@
+function onePluseAvg(x,y){
+    console.log("done")
+  return Math.round  (1+ (x+y)/2) //math.round is used to round off the number
+  //first dont write math.rount 2nd time write math.round
+}
+
+let a= 5;
+let b= 10;
+let c= 10;
+console.log("one plus average of a and b is", onePluseAvg(a,b))
+console.log ("one plus average of b and c is", onePluseAvg(b,c))
+console.log ("one plus average of a and c is", onePluseAvg(a,c))
